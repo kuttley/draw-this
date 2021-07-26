752bdb865682a3e3830f3c6c146fc29f854a3b7d
Kali Uttley <kali.uttley@healthcarousel.com>
2021-07-26 11:35:51 -0400

    package updates

--- public/client.js
+++ public/client.js
@@ -1,160 +1,171 @@
-const shake = require('shake.js');
+import shake from 'shake.js';
 
 const socket = io.connect();
 
 function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    };
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top,
+  };
 }
 
 function getRandomColor() {
-    let color = 'rgb(';
-    for (let i = 0; i < 3; i++) {
-      color += Math.floor(Math.random() * 255) + ', ';
-    }
-    color = color.slice(0, color.length-2);
-    color += ')';
-    return color;
+  let color = 'rgb(';
+  for (let i = 0; i < 3; i++) {
+    color += Math.floor(Math.random() * 255) + ', ';
+  }
+  color = color.slice(0, color.length - 2);
+  color += ')';
+  return color;
 }
 
-
 document.addEventListener('DOMContentLoaded', () => {
-  
-    // shake event handling
-    const shakeEvent = new shake({
-        threshold: 15,
-    });
-    shakeEvent.start();
-    window.addEventListener('shake', shakeEventOccur, false);
-    function shakeEventOccur() {
-        socket.emit('clear_lines', true);
+  // shake event handling
+  const shakeEvent = new shake({
+    threshold: 15,
+  });
+  shakeEvent.start();
+  window.addEventListener('shake', shakeEventOccur, false);
+  function shakeEventOccur() {
+    socket.emit('clear_lines', true);
+  }
+
+  const yourColor = getRandomColor();
+  const mouse = {
+    click: false,
+    move: false,
+    pos: { x: 0, y: 0 },
+    pos_prev: false,
+  };
+  const touch = {
+    click: false,
+    move: false,
+    pos: false,
+    pos_prev: false,
+  };
+
+  const canvas = document.getElementById('drawing');
+  const context = canvas.getContext('2d');
+  const width = window.innerWidth - 50;
+  const height = 512;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  canvas.onmousedown = (e) => {
+    mouse.pos_prev = { x: mouse.pos.x, y: mouse.pos.y };
+    mouse.click = true;
+  };
+  canvas.onmouseup = (e) => (mouse.click = false);
+  // have to normalize the mouse position to make sure canvases are using the same equal size no matter the screen size
+  canvas.onmousemove = (e) => {
+    const pos = getMousePos(canvas, e);
+    mouse.pos.x = pos.x / width;
+    mouse.pos.y = pos.y / height;
+    mouse.move = true;
+  };
+
+  // Touch Events:
+  canvas.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    touch.pos = {
+      x: e.changedTouches[0].pageX / width,
+      y: e.changedTouches[0].pageY / height - 0.25,
+    };
+    touch.click = true;
+  });
+  canvas.addEventListener('touchend', (e) => {
+    e.preventDefault();
+    touch.click = false;
+    touch.move = false;
+  });
+  canvas.addEventListener('touchmove', (e) => {
+    e.preventDefault();
+    touch.pos_prev = touch.pos;
+    touch.pos = {
+      x: e.changedTouches[0].pageX / width,
+      y: e.changedTouches[0].pageY / height - 0.25,
+    };
+    touch.move = true;
+  });
+
+  // draws every line individually.
+  // start a path, move to first point (moveTo), then draw a line to the second received point (lineTo)
+  // last call context.stroke() to actually draw the line
+  socket.on('draw_line', (data) => {
+    const line = data.line;
+    const lineColor = data.lineColor;
+    context.beginPath();
+    context.lineCap = 'round';
+    context.lineWidth = 4;
+    // line[0] == currPos, line[1] == prevPos
+    context.moveTo(line[0].x * width, line[0].y * height);
+    context.lineTo(line[1].x * width, line[1].y * height);
+    context.strokeStyle = lineColor;
+    context.stroke();
+    context.closePath();
+  });
+
+  const main = (function mainLoop() {
+    if (mouse.click && mouse.move && mouse.pos_prev) {
+      socket.emit('draw_line', {
+        line: [mouse.pos, mouse.pos_prev],
+        lineColor: yourColor,
+      });
+      mouse.move = false;
     }
-  
-    const yourColor = getRandomColor();
-    const mouse = {
-        click: false,
-        move: false,
-        pos: {x:0, y:0},
-        pos_prev: false
-    };
-    const touch = {
-        click: false,
-        move: false,
-        pos: false,
-        pos_prev: false
-    };
 
-    const canvas = document.getElementById('drawing');
-    const context = canvas.getContext('2d');
-    const width = window.innerWidth-50;
-    const height = 512;
+    if (touch.click && touch.move && touch.pos_prev) {
+      socket.emit('draw_line', {
+        line: [touch.pos, touch.pos_prev],
+        lineColor: yourColor,
+      });
+      touch.move = false;
+    }
 
-    canvas.width = width;
-    canvas.height = height;
+    mouse.pos_prev = { x: mouse.pos.x, y: mouse.pos.y };
+    setTimeout(mainLoop, 1);
+  })();
 
-    canvas.onmousedown = ((e) => { 
-        mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y}; 
-        mouse.click = true;
+  socket.on('clear_lines', () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  const clearButton = document.getElementById('clear-canvas-btn');
+  clearButton.addEventListener('click', () => {
+    socket.emit('clear_lines', true);
+  });
+
+  const saveButton = document.getElementById('save-canvas-btn');
+  saveButton.addEventListener('click', () => {
+    socket.emit('save_canvas', canvas.toDataURL('image/png'));
+    saveButton.setAttribute('disabled', true);
+    setTimeout(() => {
+      saveButton.removeAttribute('disabled');
+    }, 60000);
+  });
+
+  const loadButton = document.getElementById('load-canvas-btn');
+  loadButton.addEventListener('click', () => {
+    socket.emit('load_canvas_overlay');
+  });
+
+  socket.on('load_canvas_overlay', (canvases) => {
+    const carouselInner = document.getElementById('carousel-inner');
+    while (carouselInner.firstChild != null) {
+      carouselInner.removeChild(carouselInner.firstChild);
+    }
+
+    canvases.forEach((canvas) => {
+      const carouselItemDiv = document.createElement('div');
+      carouselItemDiv.classList.add('carousel-item', 'p-4');
+      const canvasImage = document.createElement('img');
+      canvasImage.classList.add('carousel-canvas-img', 'img-fluid', 'img-thumbnail');
+      canvasImage.src = canvas;
+      carouselItemDiv.appendChild(canvasImage);
+      carouselInner.appendChild(carouselItemDiv);
     });
-    canvas.onmouseup = ((e) => mouse.click = false);
-    // have to normalize the mouse position to make sure canvases are using the same equal size no matter the screen size
-    canvas.onmousemove = ((e) => {
-        const pos = getMousePos(canvas, e);
-        mouse.pos.x = pos.x / width;
-        mouse.pos.y = pos.y / height;
-        mouse.move = true;
-    });
-  
-    // Touch Events:
-    canvas.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-        touch.pos = {x: (e.changedTouches[0].pageX / width), y: (e.changedTouches[0].pageY / height) - .25 };
-        touch.click = true;
-    });
-    canvas.addEventListener('touchend', (e) => {
-        e.preventDefault();
-        touch.click = false;
-        touch.move = false;
-    });
-    canvas.addEventListener('touchmove', (e) => {
-        e.preventDefault();
-        touch.pos_prev = touch.pos;
-        touch.pos = {x: (e.changedTouches[0].pageX / width), y: (e.changedTouches[0].pageY / height) - .25 };
-        touch.move = true;
-    });
-
-    // draws every line individually.
-    // start a path, move to first point (moveTo), then draw a line to the second received point (lineTo)
-    // last call context.stroke() to actually draw the line
-    socket.on('draw_line', (data) => {
-        const line = data.line;
-        const lineColor = data.lineColor;
-        context.beginPath();
-        context.lineCap = 'round';
-        context.lineWidth = 4;
-        // line[0] == currPos, line[1] == prevPos
-        context.moveTo(line[0].x * width, line[0].y * height);
-        context.lineTo(line[1].x * width, line[1].y * height);
-        context.strokeStyle = lineColor;
-        context.stroke();
-        context.closePath();
-    });
-
-    const main = function mainLoop() {
-        if (mouse.click && mouse.move && mouse.pos_prev) {
-            socket.emit('draw_line', { line: [mouse.pos, mouse.pos_prev ], lineColor: yourColor });
-            mouse.move = false;
-        }
-      
-        if (touch.click && touch.move && touch.pos_prev) {
-           socket.emit('draw_line', { line: [touch.pos, touch.pos_prev], lineColor: yourColor }); 
-           touch.move = false;
-        }
-
-        mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
-        setTimeout(mainLoop, 1);
-    }();
-
-    socket.on('clear_lines', () => {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-    });
-    
-    const clearButton = document.getElementById('clear-canvas-btn');
-    clearButton.addEventListener('click', () => {
-      socket.emit('clear_lines', true);
-    });
-
-    const saveButton = document.getElementById('save-canvas-btn');
-    saveButton.addEventListener('click', () => {
-        socket.emit('save_canvas', canvas.toDataURL('image/png') );
-        saveButton.setAttribute('disabled', true);
-        setTimeout(() => {saveButton.removeAttribute('disabled')}, 60000);
-    });
-
-    const loadButton = document.getElementById('load-canvas-btn');
-    loadButton.addEventListener('click', () => {
-      socket.emit('load_canvas_overlay');
-    });
-
-    socket.on('load_canvas_overlay', (canvases) => {
-        const carouselInner = document.getElementById('carousel-inner');
-        while(carouselInner.firstChild != null) {
-            carouselInner.removeChild(carouselInner.firstChild);
-        }
-
-        canvases.forEach(canvas => {
-            const carouselItemDiv = document.createElement('div');
-            carouselItemDiv.classList.add('carousel-item', 'p-4');
-            const canvasImage = document.createElement('img');
-            canvasImage.classList.add('carousel-canvas-img', 'img-fluid', 'img-thumbnail');
-            canvasImage.src = canvas;
-            carouselItemDiv.appendChild(canvasImage);
-            carouselInner.appendChild(carouselItemDiv);
-        });
-        carouselInner.firstElementChild.classList.add('active');
-    });
-
+    carouselInner.firstElementChild.classList.add('active');
+  });
 });
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 // setting up express, http, socketIo, and the app
-const express = require('express'),
-    app = express(),
-    http = require('http'),
-    socketIo = require('socket.io'),
-    fs = require('fs'),
-    path = require('path');
+import express from 'express';
+const app = express();
+import { createServer } from 'http';
+import { writeFile, readdirSync, readFileSync } from 'fs';
+import { resolve } from 'path';
+import { Server } from 'socket.io';
+import path from 'path';
 
+const __dirname = path.resolve();
 
 // setting up the server
-const server = http.createServer(app);
-const io = socketIo.listen(server);
+const server = createServer(app);
+const io = new Server(server);
 server.listen(8080);
 
 // telling express to use the public directory for files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')));
 console.log('Server running on localhost:8080');
 
 // keeps track of lines ever drawn
@@ -25,43 +27,45 @@ const save_timeout_length = 60;
 
 // when a new client connects, socket of new client is passed as argument.
 io.on('connection', (socket) => {
-    // send all the lines that have been drawn to the new client, that way a new person to connect
-    // sees the current drawing rather than the lines drawn since joining
-    for (let i in line_history) {
-        socket.emit('draw_line', { line: line_history[i][0], lineColor: line_history[i][1] });
+  // send all the lines that have been drawn to the new client, that way a new person to connect
+  // sees the current drawing rather than the lines drawn since joining
+  for (let i in line_history) {
+    socket.emit('draw_line', { line: line_history[i][0], lineColor: line_history[i][1] });
+  }
+
+  // handler for our new message 'draw_line', each time a line is received, add it to line history and send it to
+  // connected clients so their canvas updates
+  socket.on('draw_line', (data) => {
+    line_history.push([data.line, data.lineColor]);
+    io.emit('draw_line', { line: data.line, lineColor: data.lineColor });
+  });
+
+  socket.on('clear_lines', () => {
+    line_history.splice(0);
+    io.emit('clear_lines', true);
+  });
+
+  socket.on('save_canvas', (data) => {
+    if (save_timeout_enabled === false) {
+      const canvasFile = path.join(__dirname, '/data/', Date.now() + '.png');
+      console.log(canvasFile);
+      writeFile(canvasFile, data, (err) => {
+        if (err) throw err;
+      });
+      save_timeout_enabled = true;
+      setTimeout(() => (save_timeout_enabled = false), save_timeout_length);
     }
+  });
 
-    // handler for our new message 'draw_line', each time a line is received, add it to line history and send it to
-    // connected clients so their canvas updates
-    socket.on('draw_line', (data) => {
-        line_history.push([data.line, data.lineColor]);
-        io.emit('draw_line', { line: data.line, lineColor: data.lineColor });
+  socket.on('load_canvas_overlay', () => {
+    const canvasesDir = path.join(__dirname, '/data/');
+    const savedCanvases = [];
+    const files = readdirSync(canvasesDir);
+    files.forEach((file) => {
+      if (file.endsWith('png')) {
+        savedCanvases.push(readFileSync(resolve(canvasesDir, file), 'utf8'));
+      }
     });
-
-    socket.on('clear_lines', () => {
-        line_history.splice(0);
-        io.emit('clear_lines', true);
-    });
-
-    socket.on('save_canvas', (data) => {
-        if (save_timeout_enabled === false) {
-          const canvasFile = './data/' + Date.now() + '.png';
-          console.log(canvasFile);
-          fs.writeFile(canvasFile, data, (err) => {if (err) throw err });
-          save_timeout_enabled = true;
-          setTimeout(() => save_timeout_enabled = false, save_timeout_length);
-        }
-    });
-
-    socket.on('load_canvas_overlay', () => {
-        const canvasesDir = './data/';
-        const savedCanvases = [];
-        const files = fs.readdirSync(canvasesDir);
-        files.forEach(file => {
-            if (file != '.DS_Store') {
-                savedCanvases.push(fs.readFileSync(path.resolve(canvasesDir, file), 'utf8'));
-            }
-        });
-        io.emit('load_canvas_overlay', savedCanvases);
-    });
+    io.emit('load_canvas_overlay', savedCanvases);
+  });
 });
